1d2da7990d2404227f7e6981b96c7d99e44c99a9
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-01-16 19:58:31 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Maurício Costa on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
